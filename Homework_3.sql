b4d5a2d13099efb54f2adee20776398f3a727ee3
Oğuzhan Akkoyunlu <92020160+oguzhanakk@users.noreply.github.com>
2022-10-07 14:12:30 +0300

    Update Homework_3.sql

--- Homework_3.sql
+++ Homework_3.sql
@@ -2,3 +2,14 @@
 SELECT country FROM country
 WHERE country LIKE 'A%a';
 
+--*********2.Question
+SELECT country FROM country
+WHERE country LIKE '_____%n';
+
+--*********3.Question
+SELECT title FROM film
+WHERE title ILIKE '%T%T%T%T%';
+
+--*********4.Question
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
