01cc2c0318a0e0510471dd052e7dc8d50edad2bc
Oğuzhan Akkoyunlu <92020160+oguzhanakk@users.noreply.github.com>
2022-10-07 15:28:26 +0300

    Update Homework_4.sql

--- Homework_4.sql
+++ Homework_4.sql
@@ -1,2 +1,17 @@
 --*********1.Question
+SELECT DISTINCT replacement_cost FROM film;
 
+--*********2.Question
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+--*********3.Question
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+--*********4.Question
+SELECT COUNT(country) FROM country
+WHERE country LIKE '_____';
+
+--*********5.Question
+SELECT COUNT(city) FROM city
+WHERE city ILIKE '%R';
