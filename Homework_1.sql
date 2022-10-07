4122716c6af18d713cc5ab64e1e2ba388ad46cb8
Oğuzhan Akkoyunlu <92020160+oguzhanakk@users.noreply.github.com>
2022-10-07 13:42:53 +0300

    Update Homework_1.sql

--- Homework_1.sql
+++ Homework_1.sql
@@ -1,5 +1,5 @@
 --*********1.Question
-SELECT title, description FROM film
+SELECT title, description FROM film;
 
 --*********2.Question
 SELECT * FROM film
@@ -7,7 +7,7 @@ WHERE length > 60 AND length < 75;
 
 --*********3.Question
 SELECT * FROM film
-WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99)
+WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 
 --*********4.Question
 SELECT first_name, last_name FROM customer
