7e9761c30e6538999acd27b42e373573761c548a
Oğuzhan Akkoyunlu <92020160+oguzhanakk@users.noreply.github.com>
2022-10-07 16:14:24 +0300

    Create Homework_12.sql

---
+++ Homework_12.sql
@@ -0,0 +1,34 @@
+--*********1.Question
+
+SELECT COUNT(*) FROM film
+WHERE length > 
+(SELECT AVG(length) FROM film);
+
+--*********2.Question
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate = 
+(SELECT MAX(rental_rate) FROM film);
+
+--*********3.Question
+
+SELECT * FROM film
+WHERE film_id = ANY
+(
+SELECT film_id FROM film
+WHERE 
+ rental_rate =(SELECT MIN(rental_rate) FROM film) 
+ OR 
+ replacement_cost =(SELECT MIN(replacement_cost) FROM film) 
+);	
+
+--*********4.Question
+
+SELECT customer_id,
+(SELECT first_name FROM customer 
+ WHERE payment.customer_id=customer.customer_id), 
+ (SELECT last_name FROM customer 
+ WHERE payment.customer_id=customer.customer_id),
+ COUNT(customer_id) FROM payment
+GROUP BY customer_id
+ORDER BY COUNT DESC
